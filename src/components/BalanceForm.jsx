dae83cd79a483fafdb9123c64bc904f16dc8fa22
Volodymyr Tabashniuk <60541362+vtabashniuk@users.noreply.github.com>
2025-02-28 10:46:41 +0200

    handle errors at balance form and fix balance updating at admin dashboard.

--- src/components/BalanceForm.jsx
+++ src/components/BalanceForm.jsx
@@ -6,28 +6,41 @@ import {
   DialogActions,
   Button,
   TextField,
+  Typography,
 } from "@mui/material";
 
 const BalanceForm = ({ open, onClose, student, onUpdateBalance }) => {
   const [balanceInput, setBalanceInput] = useState("");
+  const [error, setError] = useState("");
 
-useEffect(() => {
-  if (!open) {
-    setBalanceInput(""); // Очищення після закриття
-  }
-}, [open]);
+  useEffect(() => {
+    if (!open) {
+      setBalanceInput(""); // Очищення після закриття
+    }
+  }, [open]);
 
-  const handleChangeBalance = () => {
+  const handleChangeBalance = async () => {
     const newBalance = parseFloat(balanceInput);
     if (isNaN(newBalance)) return;
 
     if (student.balance + newBalance < 0) {
-      alert("Баланс не може бути менше 0.");
+      setError("Баланс не може бути менше 0");
       return;
     }
     const updatedBalance = student.balance + newBalance;
 
-    onUpdateBalance(student._id, updatedBalance);
+    try {
+      const errorMessage = await onUpdateBalance(student._id, updatedBalance);
+
+      if (errorMessage) {
+        setError(errorMessage);
+        return;
+      }
+      setError("");
+    } catch (error) {
+      setError(error?.message || "Помилка оновлення балансу");
+    }
+
     onClose();
   };
 
@@ -35,6 +48,7 @@ useEffect(() => {
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Зміна балансу</DialogTitle>
       <DialogContent>
+        {error && <Typography color="error">{error}</Typography>}
         <p>Прізвище: {student?.lastName}</p>
         <p>Ім'я: {student?.firstName}</p>
         <p>Поточний баланс: {student?.balance}</p>
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {
-  fetchUsers,
-  resetLoading,
-  clearError,
-} from "../redux/userSlice";
+import { fetchUsers, resetLoading, clearError } from "../redux/userSlice";
+import useStudentBalanceAction from "../hooks/useUserBalanceAction";
 import useUserFormAction from "../hooks/useUserFormAction";
+import BalanceForm from "../components/BalanceForm";
 import SearchField from "../components/SearchField";
 import UserForm from "../components/UserForm";
 import UsersList from "../components/UsersList";
@@ -16,6 +14,14 @@ import PersonAddTwoToneIcon from "@mui/icons-material/PersonAddTwoTone";
 const AdminDashboard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {
+    handleOpenBalanceDialog,
+    handleUpdateBalance,
+    openBalanceDialog,
+    setOpenBalanceDialog,
+    selectedStudent,
+    setSelectedStudent,
+  } = useStudentBalanceAction();
   const {
     handleEdit,
     handleUserFormSubmit,
@@ -51,9 +57,23 @@ const AdminDashboard = () => {
       <Button variant="contained" color="secondary" onClick={handleLogout}>
         Вийти
       </Button>
-      <Button variant="contained" color="primary" onClick={() => setOpenUserForm(true)}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setOpenUserForm(true)}
+      >
         <PersonAddTwoToneIcon />
       </Button>
+
+      <BalanceForm
+        open={openBalanceDialog}
+        onClose={() => {
+          setOpenBalanceDialog(false);
+          setSelectedStudent(null);
+        }}
+        student={selectedStudent}
+        onUpdateBalance={handleUpdateBalance}
+      />
 
       <UserForm
         open={openUserForm}
@@ -78,7 +98,11 @@ const AdminDashboard = () => {
       {loading ? (
         <CircularProgress />
       ) : (
-        <UsersList users={filteredUsers} onEdit={handleEdit} />
+        <UsersList
+          users={filteredUsers}
+          onEdit={handleEdit}
+          onUpdateBalance={handleOpenBalanceDialog}
+        />
       )}
     </div>
   );
